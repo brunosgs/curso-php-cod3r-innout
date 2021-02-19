a49e0ac060008051fce652bbcad00792ad44b4c6
Bruno Gomes <brunosoges@gmail.com>
2021-02-19 16:54:59 -0300

    Tela de autenticação de usuários

--- public/index.php
+++ public/index.php
@@ -1,21 +1,4 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(dirname(__FILE__, 2) . '/src/models/User.php');
-
-$user = new User([
-    'name' => 'Lucas',
-    'email' => 'lucas@cod3r.com.br'
-]);
-
-print_r(User::get(['name' => 'Chaves'], 'name, email'));
-echo '<br><br>';
-print_r(User::get([], 'name'));
-echo '<br><br>';
-
-foreach (User::get([], 'name') as $user) {
-    echo $user->name . '<br>';
-}
-
-// echo '<br>';
-// echo User::getSelect(['id' => 1], 'name, email');
+require_once(dirname(__FILE__, 2) . '/src/views/login.php');
