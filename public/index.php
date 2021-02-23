8d0f912567c8b871e7df901041e81e1ac9dbcb1f
Bruno Gomes <brunosoges@gmail.com>
2021-02-23 12:43:25 -0300

    Ajustando redirecionamento #02

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 
-$uri = urldecode($_SERVER['REQUEST_URI']);
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if ($uri === '/' || $uri === '' || $uri === '/index.php') {
     $uri = '/LoginController.php';
