f12cb6b435759f5399fd97bd2db64a62b6560e3d
Bruno Gomes <brunosoges@gmail.com>
2021-02-23 19:56:36 -0300

    Criando o cabeçalho

---
+++ src/views/template/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <span>Desenvolvido com</span>
+    <span><i class="icofont-heart text-danger mx-1"></i></span>
+    <span>por Bruno</span>
+</footer>
+</body>
+
+</html>
