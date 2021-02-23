27ace77d5295088c0a02e11470ec94ef4b07f058
Bruno Gomes <brunosoges@gmail.com>
2021-02-23 20:30:52 -0300

    Barra lateral menu

---
+++ src/views/template/menu.php
@@ -0,0 +1,34 @@
+<aside class="sidebar">
+    <nav class="menu mt-3">
+        <ul class="nav-list">
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-users mr-2">
+                        <span class="ml-2">Usuários</span>
+                    </i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="day_records.php">
+                    <i class="icofont-ui-check">
+                        <span class="ml-2">Registrar Ponto</span>
+                    </i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-ui-calendar">
+                        <span class="ml-2">Relatório Mensal</span>
+                    </i>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href=".php">
+                    <i class="icofont-chart-histogram">
+                        <span class="ml-2">Relatório Gerencial</span>
+                    </i>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
