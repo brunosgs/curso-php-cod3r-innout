db6270f30831b378b439f1242e3b7e80becafd4a
Bruno Gomes <brunosoges@gmail.com>
2021-02-24 19:44:17 -0300

    CSS template #04

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -35,8 +35,7 @@
                 <ul class="nav-list">
                     <li class="nav-item">
                         <a href="logout.php">
-                            <i class="iconfont-logout mr-2"></i>
-                            Sair
+                            <i class="icofont-logout mr-2"></i> Sair
                         </a>
                     </li>
                 </ul>
