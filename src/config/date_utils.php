614febb80f5f879041fb4955204be248a65c5624
Bruno Gomes <brunosoges@gmail.com>
2021-03-02 20:26:46 -0300

    Adicionado novas funções utilitárias de datas

--- src/config/date_utils.php
+++ src/config/date_utils.php
@@ -27,3 +27,33 @@ function getNextDay($date)
 
     return $inputDate;
 }
+
+function sumIntervals($interval1, $interval2)
+{
+    $date = new DateTime('00:00:00');
+
+    $date->add($interval1);
+    $date->add($interval2);
+
+    return (new DateTime('00:00:00'))->diff($date);
+}
+
+function subtractIntervals($interval1, $interval2)
+{
+    $date = new DateTime('00:00:00');
+
+    $date->add($interval1);
+    $date->sub($interval2);
+
+    return (new DateTime('00:00:00'))->diff($date);
+}
+
+function getDateFromInterval($interval)
+{
+    return new DateTimeImmutable($interval->format('%H:%i:%s'));
+}
+
+function getDateFromString($str)
+{
+    return DateTimeImmutable::createFromFormat('H:i:s', $str);
+}
