41f634d2408c95da30bcd7595af3149b772b6c65
Bruno Gomes <brunosoges@gmail.com>
2021-03-21 20:47:55 -0300

    Relatório gerencial

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,9 +1,45 @@
 <main class="content">
     <?php
-    echo "Quantidade de usuários: {$activeUsersCount}";
-    echo "<br>";
-    echo "Horas trabalhadas no mês: {$hoursInMonth}";
-    echo "<br>";
-    print_r($absentUsers);
+    renderTitle('Relatório Gerencial', 'Resumo das horas trabalhadas dos funcionários', 'icofont-chart-histogram');
     ?>
+
+    <div class="summary-boxes">
+        <div class="summary-box bg-primary">
+            <i class="icofont-users"></i>
+            <p class="title">Quantidade de funcionários</p>
+            <h3 class="value"><?= $activeUsersCount ?></h3>
+        </div>
+        <div class="summary-box bg-danger">
+            <i class="icofont-patient-bed"></i>
+            <p class="title">Faltas</p>
+            <h3 class="value"><?= count($absentUsers) ?></h3>
+        </div>
+        <div class="summary-box bg-success">
+            <i class="icofont-sand-clock"></i>
+            <p class="title">Horas trabalhadas no mês</p>
+            <h3 class="value"><?= $hoursInMonth ?></h3>
+        </div>
+    </div>
+    <?php if (count($absentUsers) > 0) : ?>
+    <div class="card mt-4">
+        <div class="card-header">
+            <h4 class="card-title">Faltosos do dia</h4>
+            <p class="card-category">Relação dos funcionários que ainda não bateram o ponto</p>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered table-striped table-hover">
+                <thead>
+                    <th>Nome</th>
+                </thead>
+                <tbody>
+                    <?php foreach ($absentUsers as $name) : ?>
+                    <tr>
+                        <td><?= $name ?></td>
+                    </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <?php endif ?>
 </main>
