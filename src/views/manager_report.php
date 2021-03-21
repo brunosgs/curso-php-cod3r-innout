a1007fc3e38b6963c18f8d9cc24b2031bdddac03
Bruno Gomes <brunosoges@gmail.com>
2021-03-21 20:24:09 -0300

    Consultas gerenciais #02

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -1,3 +1,9 @@
 <main class="content">
-    <h1>OK</h1>
+    <?php
+    echo "Quantidade de usuários: {$activeUsersCount}";
+    echo "<br>";
+    echo "Horas trabalhadas no mês: {$hoursInMonth}";
+    echo "<br>";
+    print_r($absentUsers);
+    ?>
 </main>
