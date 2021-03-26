d517beace140f018ac1c11bbc874c4efe92e96a0
Bruno Gomes <brunosoges@gmail.com>
2021-03-26 12:48:38 -0300

    Ajustando CSS do relatório gerencial

--- src/views/manager_report.php
+++ src/views/manager_report.php
@@ -5,17 +5,17 @@
 
     <div class="summary-boxes">
         <div class="summary-box bg-primary">
-            <i class="icofont-users"></i>
+            <i class="icon icofont-users"></i>
             <p class="title">Quantidade de funcionários</p>
             <h3 class="value"><?= $activeUsersCount ?></h3>
         </div>
         <div class="summary-box bg-danger">
-            <i class="icofont-patient-bed"></i>
+            <i class="icon icofont-patient-bed"></i>
             <p class="title">Faltas</p>
             <h3 class="value"><?= count($absentUsers) ?></h3>
         </div>
         <div class="summary-box bg-success">
-            <i class="icofont-sand-clock"></i>
+            <i class="icon icofont-sand-clock"></i>
             <p class="title">Horas trabalhadas no mês</p>
             <h3 class="value"><?= $hoursInMonth ?></h3>
         </div>
@@ -24,7 +24,7 @@
     <div class="card mt-4">
         <div class="card-header">
             <h4 class="card-title">Faltosos do dia</h4>
-            <p class="card-category">Relação dos funcionários que ainda não bateram o ponto</p>
+            <p class="card-category mb-0">Relação dos funcionários que ainda não bateram o ponto</p>
         </div>
         <div class="card-body">
             <table class="table table-bordered table-striped table-hover">
